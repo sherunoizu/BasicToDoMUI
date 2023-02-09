095054c95c958bdc351dfef24f5cd5bb71ffe03b
Nikita <nikitazapletin@gmail.com>
2023-02-09 14:39:56 +0300

    setting up prettier and eslint

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,7 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
+const App = () => (<div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -19,8 +17,10 @@ function App() {
           Learn React
         </a>
       </header>
-    </div>
-  );
-}
+    </div>)
+  
+    
+  
+
 
 export default App;
